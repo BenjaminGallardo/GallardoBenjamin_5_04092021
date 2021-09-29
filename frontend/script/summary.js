e5e518da5d314c9a3f1bad1e5861fce7e6ab0726
Gallardo Benjamin <Benjamin.Gallardo1@outlook.fr>
2021-09-29 10:31:18 +0200

    Ajout d'un bouton retour à l'accueil dans la page récapitulative

--- frontend/script/summary.js
+++ frontend/script/summary.js
@@ -3,6 +3,7 @@ viewSummary();
 function viewSummary() { // Grâce à cette fonction on réalise l'affichage sur la page de récapitlatif de commande
 
     let productInLocalStorage = JSON.parse(localStorage.getItem('products'));
+    const  sectionRecap = document.querySelector('.recap');
     const ulProductSummary = document.querySelector('.recap ul');
 
     let totalPriceInLocalStorage = JSON.parse(localStorage.getItem('priceTotal'))
@@ -19,7 +20,24 @@ function viewSummary() { // Grâce à cette fonction on réalise l'affichage sur
     priceFinal.innerHTML = ` ${totalPriceInLocalStorage}€`;
     titreTotalSummary.appendChild(priceFinal);
 
+    // Ici on va définir un numéro de commande aléatoire
+
+    const random1 = Math.random().toString(36).substr(5,15); // Math.random renvoie un nombre entre 0 et 1
+    const random2 = Math.random().toString(36).substr(5,15); 
+    const random3 = Math.random().toString(36).substr(5,15);
+
+    const numCommand = document.createElement('p');
+    numCommand.innerHTML = `<em>Identifiant de commande :</em> ${random1}-${random2}-${random3}`;
+    sectionRecap.appendChild(numCommand);
+
     localStorage.clear(); // On supprime tout le LocalStorage pour permettre 
+    
+    // Retour à l'accueil via boutton
+    
+    const btnReturn = document.querySelector('button');
+    btnReturn.addEventListener('click', function(){
+        window.location.replace("../index.html"); // Replace ne conserve pas l'historique de la page donc pas de retour possible sur la page recapitulative
+    })
 }
 
 
